2abd6d2769817e04f86f0d677be846f938bdc044
HssainAzaqy135 <kinghssain135@gmail.com>
2024-12-26 19:37:18 +0200

    Answered part1 dry

--- hw2/hw2_2024/hw2/answers.py
+++ hw2/hw2_2024/hw2/answers.py
@@ -9,28 +9,40 @@ math (delimited with $$).
 # Part 1 (Backprop) answers
 
 part1_q1 = r"""
-**Your answer:**
+Q1:
+
+A. The shape comes out to be 64x512x64x1024
+
+B. Yes, the Jacobian is sparse, because it represents how each weight in W affects the predictions. In other words, it shows how each element in the predictions is affected by the weights W. 
+For any sample i  such that 1 <= i <=64, we have that the block corresponding to how the sample Xj for some j != i affects the output Yi is completely zero. Thus, the Jacobian is effectively block diagonal with 64 blocks, therefore sparse.
+
+C.No, we do not need to materialize the Jacobian to calculate a downstream gradient with respect to the given input, $\delta\mathbf{X} = \frac{\partial L}{\partial \mathbf{X}}$. This is because the gradient can be computed using the chain rule. Specifically, $\delta\mathbf{X}$ can be obtained as $\delta\mathbf{X} = \delta\mathbf{Y} \mathbf{W}$, where $\delta\mathbf{Y} = \frac{\partial L}{\partial \mathbf{Y}}$ is the gradient of the loss with respect to the output, and $\mathbf{W}$ is the weight matrix. This avoids explicitly constructing the large Jacobian.
 
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Q2:
+
+A. The shape comes out to be 64x512x512x1024
+
+B. No the jacobian is not sparse by any garentee. Since every element of the output Y depends on all elements of W. Given W is any matrix, no sparesity is garenteed.
+
+C. No, we do no need to materialize the Jacobian to compute: $\delta\mathbf{W} = \frac{\partial L}{\partial \mathbf{W}}$. 
+We can compute $\delta\mathbf{W}$ directly using the following:
+
+$$
+\delta\mathbf{W} = \delta\mathbf{Y}^\top \mathbf{X},
+$$
+
+Again avoiding explicitly forming the Jacobian for computation.
 
 """
 
 part1_q2 = r"""
-**Your answer:**
 
+No, back-propagation is not absolutely required to train neural networks using gradient based optimization , 
+yet it makes the training way more feasible.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+Back propagation is effectively essential for gradient based optimization:** The fundamental idea behind gradient-based training is to adjust the weights of the neural network in a way that minimizes a loss function. To do this effectively, you need to compute the gradients of the loss function with respect to each of the weights in the network. Doing that the naive way would be way to slow for networks of most medium to large sizes or architectures
+
 
 """
 
