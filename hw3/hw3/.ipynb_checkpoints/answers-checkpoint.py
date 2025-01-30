a4edbc8841c7fe348f68dc79963ee28ba92ce14f
HssainAzaqy135 <kinghssain135@gmail.com>
2025-01-30 23:44:48 +0200

    finish part 1 including dry

--- hw3/hw3/.ipynb_checkpoints/answers-checkpoint.py
+++ hw3/hw3/.ipynb_checkpoints/answers-checkpoint.py
@@ -48,21 +48,36 @@ def part1_generation_params():
 part1_q1 = r"""
 **Your answer:**
 
+    We do the split for two reasons.
+    First of all, if trained on the entire corpus at once, the model will most certainly overfit. Since this is a single data point.
+    Moreover, since all the model trained for was memorizing a huge corpus, it will probably have some serious generalization issues.
+    By splitting the corpus to many smaller sequences, we get much more variety while we train the model.
 """
 
 part1_q2 = r"""
 **Your answer:**
 
+    The hidden state inside the model doesn't depend on the input sequence length. Therefore when generating text, the invoked text can be of different length than the
+    input text.
 """
 
 part1_q3 = r"""
 **Your answer:**
 
+    while training the network we pass on the hidden state between batches. This state serves as "context" from the previous samples in the batch. This forces us to send
+    batches that contain sentences from the same context, hoping that text that is locally close is of the same context generally.
+    We do this to train the model for using the hidden state as previous context and not some random value.
 """
 
 part1_q4 = r"""
 **Your answer:**
-
+1. The temperature controls how uniform or "spiky" the diffusion of the distribution is. The hotter the more uniform, the colder the more "spiky".Since we aim to use the model to pick the most likely character to come next, we would want the "spiky" distribution, so we use lower temperature which allows
+shifting the probability of choosing the right character to be higher and lower other probabilities.
+    
+2. When the temperature is very high, the distribution becomes uniform, since from the temperature softmax formula we divide all exponents by T. Exponents of numbers close to zero are 1, making all exponents pretty much the same. This is what results in uniform behavior.
+    
+3. When dividing the exponent inputs by low T, we are making them larger. This, together with the fact they are inputs of exponents, allows us to exaggerate minor differences even more. Resulting in a more "spiky" distribution which allows us to make "stronger" / "more confident" choices with the outputted probabilities.
+    
 
 """
 # ==============
